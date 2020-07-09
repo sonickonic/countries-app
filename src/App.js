5b9a79269909188fcaf24c59829ef1114dc73053
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-07-09 18:06:13 +0100

    Integrate REST Countries API
    - Pull country data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const baseEndpoint = "https://restcountries.eu/rest/v2/all";
+
+async function fetchCountries() {
+  const response = await fetch(`${baseEndpoint}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchCountries();
+
 function App() {
   return <div className="App"></div>;
 }
