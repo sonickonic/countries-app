49f5b7daaa6d95327b090b94b15a418208cd4ca6
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-07-10 18:09:02 +0100

    Add Header component
    - Toggle the color scheme between light and dark mode

---
+++ src/Header.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./Header.scss";
+
+function Header() {
+  const [theme, setTheme] = useState("light");
+
+  function handleClick(e) {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+
+    if (e.target.checked) {
+      transition();
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      transition();
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }
+
+  function transition() {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+      document.documentElement.classList.remove("transition");
+    }, 1000);
+  }
+
+  return (
+    <div className="header">
+      <div className="header__container">
+        <h1 className="header__title">Where in the world?</h1>
+        <div className="header__toggle-container">
+          <input
+            onClick={handleClick}
+            className="checkbox"
+            type="checkbox"
+            id="toggle"
+          />
+          <label className="switch" htmlFor="toggle">
+            <i className={theme === "dark" ? "fas fa-moon" : "far fa-moon"} />
+          </label>
+          <p className="header__dark-mode">Dark Mode</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
