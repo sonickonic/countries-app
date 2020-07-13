ff2277adba9b07afb505dd37b107b9ba56ef746e
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-07-13 16:36:40 +0100

    Add Select box component
    - Filter countries by region

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Card from "./Card";
+import SelectBox from "./SelectBox";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const baseEndpoint = "https://restcountries.eu/rest/v2/all";
+  const [selected, setSelected] = useState("Filter by Region");
 
   async function fetchCountries() {
     const response = await fetch(`${baseEndpoint}`);
@@ -16,12 +18,28 @@ function App() {
     fetchCountries();
   }, []);
 
+  function handleClick(e) {
+    setSelected(e.target.innerText);
+  }
+
+  function matchSelected(country) {
+    if (selected === "Filter by Region") return true;
+    if (selected === "All") {
+      setSelected("Filter by Region");
+    } else {
+      return country.region.includes(selected);
+    }
+  }
+
   return (
     <div className="App">
       <Header />
-      {countries.map((country, index) => (
-        <Card key={index} country={country} />
-      ))}
+      <SelectBox handleClick={handleClick} selected={selected} />
+      {countries
+        .filter((country) => matchSelected(country))
+        .map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
     </div>
   );
 }
