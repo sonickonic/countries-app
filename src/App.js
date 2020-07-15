4b1508f2a5e2d1d931403f1f49d75d8c7cbcad53
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-07-15 19:20:12 +0100

    Add routing feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import ListView from "./ListView";
 import Header from "./Header";
+import DetailsView from "./DetailsView";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const baseEndpoint = "https://restcountries.eu/rest/v2/all";
+
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
 
   async function fetchCountries() {
     const response = await fetch(`${baseEndpoint}`);
@@ -15,11 +32,22 @@ function App() {
   useEffect(() => {
     fetchCountries();
   }, []);
+
   return (
-    <div>
-      <Header />
-      <ListView countries={countries} />
-    </div>
+    <Router>
+      <ScrollToTop />
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <ListView countries={countries} />
+          </Route>
+          <Route path="/:id">
+            <DetailsView countries={countries} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
