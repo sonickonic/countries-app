9b4f62457563c9a0e76bc54b6cbe8e664f6588f6
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-07-17 12:12:34 +0100

    Updates

---
+++ precache-manifest.48df0548095e047c66da058a8d695ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daab60c42dbe5f4670d98231597bece2",
+    "url": "/countries-api/index.html"
+  },
+  {
+    "revision": "223e357ca3956944a514",
+    "url": "/countries-api/static/css/main.c03d7b45.chunk.css"
+  },
+  {
+    "revision": "c6a82842aaff339c8813",
+    "url": "/countries-api/static/js/2.c9dbc703.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countries-api/static/js/2.c9dbc703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223e357ca3956944a514",
+    "url": "/countries-api/static/js/main.767678c8.chunk.js"
+  },
+  {
+    "revision": "d51b6a52f40df57319d5",
+    "url": "/countries-api/static/js/runtime-main.ec8c73de.js"
+  }
+]);
